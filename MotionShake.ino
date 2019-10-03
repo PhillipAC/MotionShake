61fb376f12de6034f42cc689a951af6b2df6a121
Phillip Clontz <PhillipAC718@gmail.com>
2019-10-03 18:06:54 -0500

    Updated to use a Servo motor instead of a vibrating motor for more control over movement.

--- MotionShake.ino
+++ MotionShake.ino
@@ -1,41 +1,41 @@
-//Device will detect motion at which point it will cycle through vibrator motors for one second
-//each. After the duration is reached the device will pause for the duration of the cool down.
+//Device will detect motion at which point it will move a motor between 0 degress 
+//and 180 degrees every half second. 
 
-
+#include <Servo.h>
+Servo servo;
+ 
 int sensor = 2; //The pin the sensor is connected to
-int vibrators[] = {11,12}; //The pins the vibrator motors are set to
-int vibLength = sizeof(vibrators)/sizeof(int); //How many vibrator motors exist
+int servoPin = 12; //The pins the motor is set to
+int motorPositions[] = {0,180}; //The extreme positions of the motor for shaking
 int val = 0; //The read from the sensor
-int duration = 6; //How many seconds the vibrations will last in total
-int coolDown = 5; //How many seconds the device will wait before look for motion
+int duration = 10; //How many seconds the vibrations will last in total
+int coolDown = 10; //How many seconds the device will wait before look for motion
 
 void setup() {
-  //Set each vibrartor pin to output
-  for(int i = 0; i < vibLength; i++)
-  {
-    pinMode(vibrators[i], OUTPUT);
-  }
+  //Set the pin for the motor
+  servo.attach(servoPin);
   //Set the sensor pin to input
   pinMode(sensor, INPUT);  
+  servo.write(90);
 }
 
-void loop(){
-  //Check teh sensor
+void loop(){ 
+  //Check the sensor
   val = digitalRead(sensor);
   //If the sensor returns HIGH begin to vibrate 
   if (val == HIGH) {
-    vibrate(6);
+    vibrate(duration);
     //Allow time before the vibration can be triggered again
-    delay(5000);          
+    delay(coolDown * 1000);          
   } 
 }
 
 void vibrate(int totalDuration){
-  int pin = 0;
-  for(int i = 0; i < totalDuration; i++){
-    pin = i % vibLength;
-    digitalWrite(vibrators[pin], HIGH);
-    delay(1000);
-    digitalWrite(vibrators[pin], LOW);  
+  int index = 0;
+  for(int i = 0; i < totalDuration * 2; i++){
+    index = i % 2;
+    servo.write(motorPositions[index]);
+    delay(500);  
   }
+  servo.write(90);
 }
